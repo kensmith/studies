4523e5f0eb8caa598e579ad77dc00871714015bd
Ken Smith <kgsmith@gmail.com>
2013-01-22 14:37:29 -0800

    eh

---
+++ static-consts/hi.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+struct foo_t
+{
+   enum yo
+   {
+      a = 26,
+      b = 40,
+      c = 1,
+   };
+   static const yo x = c;
+};
---
+++ static-consts/main.cpp
@@ -0,0 +1,7 @@
+#include "hi.hpp"
+#include <iostream>
+
+int main()
+{
+   foo_t::yo val = foo_t::x;
+}
