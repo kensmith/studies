761c55031e5b11a4b6c2ddb51897a957d4352597
Ken Smith <kgsmith@gmail.com>
2013-09-18 09:43:07 -0700

    bootstrapping markov

---
+++ 4e_/markov_t.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstddef>
+
+template <std::size_t chain_length>
+struct markov_t
+{
+};
---
+++ 4e_/test-markov_t.cpp
@@ -0,0 +1,8 @@
+#include ".test.hpp"
+#include "markov_t.hpp"
+
+TEST(basic)
+{
+   markov_t<2> markov;
+   (void) markov;
+}
