2d9282b9149adbf26b5dce6b39602fbf9342aaa4
Ken Smith <kgsmith@gmail.com>
2014-10-02 00:32:24 -0700

    nonempty test case

--- cppandbeyond2014/test-variable-templates.cpp
+++ cppandbeyond2014/test-variable-templates.cpp
@@ -3,9 +3,9 @@
 #if 0 // gcc 4.9.1 apparently not ready
 template<typename T>
 constexpr T pi = T(3.1415926535897932385);
+#endif
 
 TEST(canoncial_pi)
 {
    //auto x = pi<float> * 1.0f;
 }
-#endif
