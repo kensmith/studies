f99100f94495540fbbf1496acb22af325c8fe002
Ken Smith <kgsmith@gmail.com>
2014-10-04 18:19:33 -0700

    sanitizing

--- cppandbeyond2014/test-quoted.cpp
+++ cppandbeyond2014/test-quoted.cpp
@@ -4,9 +4,9 @@
 
 TEST(without_quoting)
 {
-   std::stringstream ss;
+   std::stringstream ss{};
    std::string original = "foolish me";
-   std::string round_trip;
+   std::string round_trip = "";
    ss << original;
    ss >> round_trip;
    NE(original, round_trip);
@@ -14,9 +14,9 @@ TEST(without_quoting)
 
 TEST(with_quoting)
 {
-   std::stringstream ss;
+   std::stringstream ss{};
    std::string original = "foolish me";
-   std::string round_trip;
+   std::string round_trip = "";
    ss << std::quoted(original);
    ss >> std::quoted(round_trip);
    EQ(original, round_trip);
