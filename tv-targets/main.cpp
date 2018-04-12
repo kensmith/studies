5adc6be0c4ac94d1b912fb0f585daff8485cee84
Ken Smith <kgsmith@gmail.com>
2018-04-11 20:49:28 -0700

    finer calibration image

--- tv-targets/main.cpp
+++ tv-targets/main.cpp
@@ -7,9 +7,31 @@ const int width = 1920;
 
 void draw_vert(im::Image& image, int x, im::Color color)
 {
+#if 0
   for (int i = 0; i < height; ++i)
   {
     image.pixelColor(x, i, color);
+  }
+#else
+  auto line = im::DrawableLine(x, 0, x, height);
+  image.draw(line);
+#endif
+}
+
+void draw_calibration_image(im::Image& image)
+{
+  auto black = im::Color("black");
+  auto red = im::Color("red");
+  auto yellow = im::Color("yellow");
+  const int skip = 2;
+  for (int x = 0; x < width/skip; ++x)
+  {
+    auto color = black;
+    if (x % skip == 0)
+    {
+      color = yellow;
+    }
+    draw_vert(image, x*skip, color);
   }
 }
 
@@ -17,17 +39,8 @@ int main()
 {
   im::Image base(im::Geometry(width, height), "white");
   base.magick("GIF");
-  auto black = im::Color("black");
-  auto red = im::Color("red");
-  for (int x = 0; x < width/5; ++x)
-  {
-    auto color = black;
-    if (x % 5 == 0)
-    {
-      color = red;
-    }
-    draw_vert(base, x*5, color);
-  }
+  base.strokeAntiAlias(false);
+  draw_calibration_image(base);
   base.write("calibration.gif");
   return 0;
 }
