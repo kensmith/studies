2bea9d7aff04a3520642bd65ac83c3db0a5bce00
Ken Smith <fwxgq.oysqv@fastmail.com>
2024-12-21 23:28:15 -0800

    quick and dirty battery notifier for screen

--- gobatt/go.mod
+++ gobatt/go.mod
@@ -2,4 +2,14 @@ module github.com/kensmith/gobatt
 
 go 1.23.4
 
-require golang.org/x/sys v0.28.0
+require (
+	github.com/gen2brain/beeep v0.0.0-20240516210008-9c006672e7f4
+	golang.org/x/sys v0.28.0
+)
+
+require (
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+)
--- gobatt/main.go
+++ gobatt/main.go
@@ -1,30 +1,116 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gen2brain/beeep"
 
 	"github.com/kensmith/gobatt/sys"
 )
 
-var Batterizers = map[string]func() float64{
-	"Linux": func() float64 {
-		slog.Info("batterizer 100 for linux")
-		return 0.0
+const (
+	batterySysFileGlob       = "/sys/bus/acpi/drivers/battery/PNP0C0A:0*/power_supply/*"
+	lowBatteryThreshold      = 15
+	criticalBatteryThreshold = 5
+)
+
+var lowBatteryGate = false
+var criticalBatteryGate = false
+
+func assertPath(path string) string {
+	glob, err := filepath.Glob(path)
+	if err != nil || len(glob) <= 0 {
+		panic("path assertion failed: " + path)
+	}
+	return glob[0]
+}
+
+func readString(path string) string {
+	contents, err := os.ReadFile(path)
+	if err != nil || len(contents) <= 0 {
+		panic("couldn't read contents of file")
+	}
+	s := strings.Trim(string(contents), " \n\t\r")
+	return s
+}
+
+func assertString(path string) string {
+	return readString(assertPath(path))
+}
+
+func readInt(path string) int {
+	s := readString(path)
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		panic("couldn't convert file contents to int")
+	}
+	return val
+}
+
+func assertInt(path string) int {
+	return readInt(assertPath(path))
+}
+
+// these should be platform specific ways to get the current battery
+// fullness as a number between 0 and 1 and the discharging state (true =
+// discharging)
+var Batterizers = map[string]func() (float64, bool){
+	"Linux": func() (float64, bool) {
+		baseDir := assertPath(batterySysFileGlob)
+		energyNow := assertInt(baseDir + "/energy_now")
+		energyFull := assertInt(baseDir + "/energy_full")
+		batt := float64(energyNow) / float64(energyFull)
+		statusString := assertString(baseDir + "/status")
+		status := statusString == "Discharging"
+
+		return batt, status
 	},
 }
 
 func main() {
-	if sys.Uname() == "Linux" {
-		slog.Info("yes")
-	} else {
-		slog.Info("no")
-	}
-
 	uname := sys.Uname()
 	batterizer, ok := Batterizers[uname]
 	if !ok {
 		slog.Error("no batterizer implemented", "uname", uname)
 		panic(uname)
 	}
-	slog.Info("asdf", "batterizer", batterizer())
+	for {
+		ratio, discharging := batterizer()
+		percentage := math.Round(ratio * 100)
+		msg := fmt.Sprintf("%v%%", percentage)
+		if discharging {
+			msg = fmt.Sprintf("{%v}\n", msg)
+		} else {
+			msg = fmt.Sprintf("[%v]\n", msg)
+		}
+		print(msg)
+
+		if percentage <= criticalBatteryThreshold && !criticalBatteryGate {
+			if discharging {
+				critMsg := fmt.Sprintf("battery is critical: %v%%", percentage)
+				criticalBatteryGate = true
+				_ = beeep.Alert("battery", critMsg, "")
+			}
+		} else if percentage <= lowBatteryThreshold && !lowBatteryGate {
+			if discharging {
+				lowMsg := fmt.Sprintf("battery is low: %v%%", percentage)
+				lowBatteryGate = true
+				_ = beeep.Notify("battery", lowMsg, "")
+			}
+		} else {
+			if !discharging {
+				criticalBatteryGate = false
+				lowBatteryGate = false
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+	}
 }
