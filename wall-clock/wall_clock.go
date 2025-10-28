0b1bfc540e3342f2e52627618cdb3d89944e408c
Ken Smith <fwxgq.oysqv@fastmail.com>
2025-10-28 09:17:25 -0700

    example

--- wall-clock/wall_clock.go
+++ wall-clock/wall_clock.go
@@ -34,9 +34,15 @@ func NewWallclock() *Wallclock {
 
 // Usage:
 // at the top of any closure:
-// defer debug.NewWallclock().LogDuration()
+// defer debug.NewWallclock().LogDuration()V
 // this will slog duration_ms during the defer (exit from closure) to measure wallclock time consumed by the closure
 // the source file/line of the caller will be displayed, if possible, rather than of this function.
+// to isolate to a subpart of a function, just open a new closure:
+//
+//	func() {
+//			defer debug.NewWallclock().LogDuration()
+//			...
+//	}()
 func (wc *Wallclock) LogDuration() {
 	durationMs := float64(time.Since(wc.startTime).Nanoseconds()) / 1e6
 	slog.Info("Wallclock", slog.SourceKey, wc.src, "duration_ms", durationMs)
