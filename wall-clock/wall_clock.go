ef208ab957de6232bce2781fe6c193d5eddb167f
Ken Smith <fwxgq.oysqv@fastmail.com>
2025-10-28 09:16:08 -0700

    clarification

--- wall-clock/wall_clock.go
+++ wall-clock/wall_clock.go
@@ -36,6 +36,7 @@ func NewWallclock() *Wallclock {
 // at the top of any closure:
 // defer debug.NewWallclock().LogDuration()
 // this will slog duration_ms during the defer (exit from closure) to measure wallclock time consumed by the closure
+// the source file/line of the caller will be displayed, if possible, rather than of this function.
 func (wc *Wallclock) LogDuration() {
 	durationMs := float64(time.Since(wc.startTime).Nanoseconds()) / 1e6
 	slog.Info("Wallclock", slog.SourceKey, wc.src, "duration_ms", durationMs)
